59583faaefa4e255aa9e9693d7fb251b35c2db2a
Jeremy Jacobs <j4c0bs@users.noreply.github.com>
2020-05-16 16:09:47 -0700

    Full run tests

---
+++ tests/test_spectrum_schema/nested_reserved.sql
@@ -0,0 +1,36 @@
+CREATE EXTERNAL TABLE {schema}.{table} (
+    uid VARCHAR,
+    data struct<
+        "user": VARCHAR,
+        events: array<
+            struct<
+                "timestamp": SMALLINT,
+                a: BOOL
+            >,
+            struct<
+                "timestamp": SMALLINT,
+                a: BOOL
+            >,
+            struct<
+                "timestamp": SMALLINT,
+                a: BOOL
+            >
+        >,
+        nested_reserved: struct<
+            "column": SMALLINT,
+            "table": SMALLINT
+        >
+    >,
+    "test-underscore" SMALLINT
+)
+ROW FORMAT SERDE
+    'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+    'case.insensitive'='TRUE',
+    'ignore.malformed.json'='TRUE'
+)
+STORED AS INPUTFORMAT
+    'org.apache.hadoop.mapred.TextInputFormat'
+OUTPUTFORMAT
+    'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION 's3://{bucket}/{prefix}';
